49563d54d598f3a9789a8da6cd9e25b12ca38292
Nikkey-Liu <1125636300@qq.com>
2020-03-26 02:32:47 +0800

    bug

--- src/main/java/cn/wan/owl/dto/CartDto.java
+++ src/main/java/cn/wan/owl/dto/CartDto.java
@@ -15,7 +15,7 @@ public  class CartDto
 public Cart setCart(NProduct nProduct){
     Cart cart= new Cart();
     cart.setPrice(nProduct.getPrice());
-    cart.setQuantity(nProduct.getQuantity());
+    cart.setQuantity(1);
     cart.setProductid(nProduct.getProductid());
     cart.setProductname(nProduct.getProductname());
     cart.setUserid(UserUtil.getCurrentUser().getUserid());
