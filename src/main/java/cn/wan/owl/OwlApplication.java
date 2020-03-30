cf7913f60df4f3573698fb32cbbee660526c7445
Nikkey-Liu <1125636300@qq.com>
2020-03-31 05:49:49 +0800

    delet

--- src/main/java/cn/wan/owl/OwlApplication.java
+++ src/main/java/cn/wan/owl/OwlApplication.java
@@ -4,10 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @Author: wanhongji
- * @Date: 2020/2/7
- */
 
 @SpringBootApplication
 //@EnableScheduling
