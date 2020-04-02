08364cb8f818c2d2dfb10e5fc95a60fb6b498dc5
Gowrish Shenoy <34574855+shenoygowrish@users.noreply.github.com>
2020-04-03 00:06:36 +0100

    Update Cart.java

---
+++ src/main/java/cn/wan/owl/model/Cart.java
@@ -0,0 +1,70 @@
+package cn.wan.owl.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+    private Integer cartid;
+
+    private Integer productid;
+
+    private String productname;
+
+    private Integer userid;
+
+    private Integer price;
+
+    private Integer quantity;
+
+    public Integer getCartid() {
+        return cartid;
+    }
+
+    public void setCartid(Integer cartid) {
+        this.cartid = cartid;
+    }
+
+    public Integer getProductid() {
+        return productid;
+    }
+
+    public void setProductid(Integer productid) {
+        this.productid = productid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
