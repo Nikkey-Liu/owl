b6aa08be9290fbb120a1701a083453ab887be0bc
Yashaswy <yashaswya@users.noreply.github.com>
2020-04-09 21:13:42 +0100

    updated logic

---
+++ src/main/java/cn/wan/owl/model/NProduct.java
@@ -0,0 +1,87 @@
+package cn.wan.owl.model;
+
+import cn.wan.owl.dto.pdf.IReportStructure;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import lombok.AllArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NProduct {
+    private Integer productid;
+
+    private String productname;
+
+    private String productdescription;
+
+    private Integer quantity;
+
+    private Integer price;
+
+    private Integer userid;
+
+    private String productstate;
+
+    public Integer getProductid() {
+        return productid;
+    }
+
+    public void setProductid(Integer productid) {
+        this.productid = productid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public String getProductdescription() {
+        return productdescription;
+    }
+
+    public void setProductdescription(String productdescription) {
+        this.productdescription = productdescription;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getProductstate() {
+        return productstate;
+    }
+
+    public void setProductstate(String productstate) {
+        this.productstate = productstate;
+    }
+
+
+}
