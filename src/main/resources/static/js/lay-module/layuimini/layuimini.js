c9e744e3fa32cd681344d5e613a4a4d90c0ba7b9
Gowrish Shenoy <34574855+shenoygowrish@users.noreply.github.com>
2020-04-12 18:48:31 +0100

    Html files updated

--- src/main/resources/static/js/lay-module/layuimini/layuimini.js
+++ src/main/resources/static/js/lay-module/layuimini/layuimini.js
@@ -141,7 +141,7 @@ layui.define(["element", "jquery", "layer"], function (exports) {
                             }
                         };
                         addMenuClass($(this).parent(), 1);
-                        var pageHeaderHtml = '<a lay-href href="/">主页</a><span lay-separator="">/</span>\n';
+                        var pageHeaderHtml = '<a lay-href href="/">Home </a><span lay-separator="">/</span>\n';
                         for (var i = pageHeader.length - 1; i >= 0; i--) {
                             pageHeader[i] = pageHeader[i].replace(/\s+/g, "");
                             if (i != 0) {
